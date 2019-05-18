0ec4c03b94baf598eb983115a6f043c1413a25fa
Bnieuwenhuijs <bart@nieuwen.nl>
2019-05-18 20:12:50 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,14 +2,11 @@ from flask import render_template
 from app import app
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return "hello world"
+    user = {'username': 'Miguel'}
+    return render_template('index.html')
 
-#@app.route('/index')
-#def index():
-#    user = {'username': 'Miguel'}
-#    return render_template('index.html')
-
-#@app.route('/login')
-#def login():
-#    return render_template('Login.html')
+@app.route('/login')
+def login():
+    return render_template('Login.html')
