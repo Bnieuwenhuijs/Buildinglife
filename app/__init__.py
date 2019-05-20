d93086fda617d94505e01bfcb4debe374079e77a
Bnieuwenhuijs <bart@nieuwen.nl>
2019-05-20 16:11:24 +0200

    Revert "Revert "Add DB""
    This reverts commit b2c65b78ee17ee07e67f734bcce4d412fa6678f3.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bcvuhfokdvquhx:dd1f665b71b0fb8fd677412383da95856fbb5cd3a2c10716ff5aa2b1fb0ba98a@ec2-23-23-228-132.compute-1.amazonaws.com:5432/damf0i63c60khm'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import routes
