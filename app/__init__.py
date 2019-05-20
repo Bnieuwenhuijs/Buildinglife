6d42adcde8e08811fc56475a20e67e994abe2fa6
Bnieuwenhuijs <bart@nieuwen.nl>
2019-05-20 16:15:57 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bcvuhfokdvquhx:dd1f665b71b0fb8fd677412383da95856fbb5cd3a2c10716ff5aa2b1fb0ba98a@ec2-23-23-228-132.compute-1.amazonaws.com:5432/damf0i63c60khm'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'DATABASE_URL'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
