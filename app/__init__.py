f52f4bb9c43c4183b2e5d5ff7e7b3bf8eaae8d5e
Bnieuwenhuijs <bart@nieuwen.nl>
2019-05-20 14:51:45 +0200

    added DB boilerplate

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from config import Config
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 app.config.from_object(Config)
 
 from app import routes
