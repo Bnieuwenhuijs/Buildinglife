b04d8600df255014c45c469419977f295e6ffc64
Antoine Lyonnet <a.lyonnet@students.uu.nl>
2019-05-31 13:22:34 +0200

    login working with ugly buttons

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
 from app import db
+from flask_login import UserMixin
+
 
 class Building(db.Model):
     __tablename__ = "Building"
 
-    id                      = db.Column(db.Integer, primary_key=True)    
+    id                      = db.Column(db.Integer, primary_key=True)
     building_year           = db.Column(db.Integer)
     building_functionality  = db.Column(db.String(120))
     square_meters           = db.Column(db.Float)
@@ -21,6 +23,18 @@ class Building(db.Model):
     glass_Value             = db.Column(db.Integer)
     polystyrene_quantity    = db.Column(db.Integer)
     polystyrene_Value       = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Building {}>'.format(self.building_year)
+
+class User(UserMixin, db.Model):
+    id                      = db.Column(db.Integer, primary_key=True)
+    name                    = db.Column(db.String(120))
+    surname                 = db.Column(db.String(120))
+    username                = db.Column(db.String(64), index=True, unique=True)
+    email                   = db.Column(db.String(120), index=True, unique=True)
+    password_hash           = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
