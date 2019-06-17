a97f3c88dcf229de31ce0ad6141a37289f14d745
Bnieuwenhuijs <bart@nieuwen.nl>
2019-06-17 08:00:57 +0200

    data

---
+++ app/generate_data.py
@@ -0,0 +1,30 @@
+from Building_information_api import get_building_properties
+import pandas as pd 
+import csv
+
+building = []
+
+n = 42
+while n < 49:
+    building_properties = get_building_properties('7813AA', str(n), False)
+    building_properties['windows'] = 5
+    building.append(building_properties)
+    n = n + 1
+
+
+n = 2
+while n < 11:
+    building_properties = get_building_properties('6051MW', str(n), False)
+    building_properties['windows'] = 5
+    building.append(building_properties)
+    n = n + 2
+
+n = 19
+while n < 25:
+    building_properties = get_building_properties('6051HN', str(n), False)
+    building_properties['windows'] = 5
+    building.append(building_properties)
+    n = n + 2
+
+df = pd.DataFrame(building) 
+export_csv = df.to_csv (r'C:\Users\Bart Nieuwenhuijs\Documents\GitHub\Buildinglife\app\export_dataframe.csv', index = None, header=True)
