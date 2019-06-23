69e8599694d29809466cb01d4f23b536f4cd1352
Thegamebaker <rick.brouwers@live.nl>
2019-06-24 00:09:12 +0200

    Revert "Merge branch 'Rick' of https://github.com/Bnieuwenhuijs/Buildinglife into Rick"
    This reverts commit c4a1eaaa9e43dc5c870011607e5b9af7dd8608a4, reversing
    changes made to ccf4fe4a509e6e6e0b1cf941e577a05d906d59b7.

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config(object):
 	SECURITY_PASSWORD_SALT = 'my_precious_two'
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-		os.environ.get('DATABASE_URL_local')
+		'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
