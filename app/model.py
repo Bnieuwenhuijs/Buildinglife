2e852eee4c793253dd11f83f787de287b04bec49
Thegamebaker <rick.brouwers@live.nl>
2019-06-25 00:21:25 +0200

    Last fixes

--- app/model.py
+++ app/model.py
@@ -11,14 +11,15 @@ import json
 # Industriefunctie = 3
 # anders = 4
 
-
-
-dataset = pd.read_csv('D:\Data for Model\completed.csv')
     # Steel = 8
     # Windows not included = 1:7
     # Windows included = 1:8
-y = dataset.iloc[:, 12].values
-X = dataset.iloc[:, 1:7].values
+
+dataset = pd.read_csv('D:\Data for Model\completed.csv')
+X = dataset.iloc[:, 1:8].values
+
+# --- STEEL ----
+y = dataset.iloc[:, 8].values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
@@ -29,9 +30,73 @@ y_pred = regressor.predict(X_test)
 
 #df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
 
-filename = 'finalized_model.sav'
+filename = 'steel_window_model.sav'
 pickle.dump(regressor, open(filename, 'wb'))
 
-#pickle.dump(regressor, open('steel.p','wb'))
+# --- Copper ----
+y = dataset.iloc[:, 9].values
 
-#model = pickle.load(open('steel.pkl','rb'))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+filename = 'copper_window_model.sav'
+pickle.dump(regressor, open(filename, 'wb'))
+
+# --- Timber ---
+y = dataset.iloc[:, 10].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+filename = 'timber_window_model.sav'
+pickle.dump(regressor, open(filename, 'wb'))
+
+# --- Concrete ----
+y = dataset.iloc[:, 11].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+filename = 'concrete_window_model.sav'
+pickle.dump(regressor, open(filename, 'wb'))
+
+# --- Glass ---
+
+y = dataset.iloc[:, 12].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+filename = 'glass_window_model.sav'
+pickle.dump(regressor, open(filename, 'wb'))
+
+# --- Polystyrene
+
+y = dataset.iloc[:, 13].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+filename = 'polystyrene_window_model.sav'
+pickle.dump(regressor, open(filename, 'wb'))
+
