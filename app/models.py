537d4fb3574e3ca89e46a08fda97433510eba054
Bnieuwenhuijs <bart@nieuwen.nl>
2019-06-25 10:46:14 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,6 +3,19 @@ from flask_login import UserMixin
 from datetime import datetime
 import hashlib, enum
 
+class User(UserMixin, db.Model):
+	__tablename__ = "user"
+
+	id 						= db.Column(db.Integer, primary_key=True)
+	name 					= db.Column(db.String(120))
+	surname 				= db.Column(db.String(120))
+	username 				= db.Column(db.String(64), index=True, unique=True)
+	email 					= db.Column(db.String(120), index=True, unique=True)
+	password_hash 			= db.Column(db.String(128))
+	isConfirmed 			= db.Column(db.Boolean, default = False)
+
+	def __repr__(self):
+		return '<User {}>'.format(self.username)
 
 class Building(db.Model):
 	__tablename__ = "Building"
@@ -50,21 +63,6 @@ class Material_estimation(db.Model):
 
 	def __repr__(self):
 		return '<Building {}>'.format(self.building_year)
-
-
-class User(UserMixin, db.Model):
-	__tablename__ = "user"
-
-	id 						= db.Column(db.Integer, primary_key=True)
-	name 					= db.Column(db.String(120))
-	surname 				= db.Column(db.String(120))
-	username 				= db.Column(db.String(64), index=True, unique=True)
-	email 					= db.Column(db.String(120), index=True, unique=True)
-	password_hash 			= db.Column(db.String(128))
-	isConfirmed 			= db.Column(db.Boolean, default = False)
-
-	def __repr__(self):
-		return '<User {}>'.format(self.username)
 
 class LicenseType(enum.Enum):
 	STARTER		 = "Starter"
